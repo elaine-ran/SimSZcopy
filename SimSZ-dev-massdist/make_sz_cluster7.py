0d548fa5184fe8db54b0233124b41c5a88e72a3f
Elaine Ran <elaineran@MacBook-Air-4.local>
2023-06-14 00:39:50 -0400

    plotted NFW profile

--- SimSZ-dev-massdist/make_sz_cluster7.py
+++ SimSZ-dev-massdist/make_sz_cluster7.py
@@ -13,32 +13,31 @@ class GenerateCluster():
 
     def __init__(self):
         self.data =[]
-        
-        
+
+
     def P200_Battaglia2012(self,cosmo,z,M200,R200):
-        
-        P200 = G * M200*u.Msun * 200. * cosmo.critical_density(z) * (cosmo.Ob0/cosmo.Om0) / (2. * R200*u.Mpc) #From Battaglia 2012
-        P200=P200.to(u.keV/u.cm**3.) #Unit conversion to keV/cm^3
-        
+
+        P200 = (G*M200 * 200. * cosmo.critical_density(z) * (cosmo.Ob0/cosmo.Om0)) / (2. * R200) #From Battaglia 2012
+
         return(P200)
-    
+
     def param_Battaglia2012(self,A0,alpha_m,alpha_z,M200,z):
-        
-        A = A0 * (M200/1e14)**alpha_m * (1.+z)**alpha_z
+
+        A = A0 * ((M200/1e14)**alpha_m) * (1.+z)**alpha_z
 
         return(A)
-    
+
     def Pth_Battaglia2012(self,cosmo,r,z,R200,gamma,alpha,beta,xc,P0,P200,M200):
-        
+
         x=r/R200
-        
+
         Pth = P0 * (x/xc)**gamma * (1+(x/xc)**alpha)**(-beta)
-        
+
         return(Pth)
-  
 
 
-#FUNCTIONS BELOW HERE HAVE NOT BEEN TESTED OR USED RECENTLY     
+
+#FUNCTIONS BELOW HERE HAVE NOT BEEN TESTED OR USED RECENTLY
 
 
 
@@ -74,7 +73,7 @@ class GenerateCluster():
         disk_mean = Map[r < rin].mean()
         ring_mean = Map[(r >= rin) & (r < rout)].mean()
         tSZ = disk_mean - ring_mean
-        
+
         return tSZ, rin
 
 
@@ -105,34 +104,34 @@ class GenerateCluster():
 
     def battaglia_profile(r, Mvir, z, cosmo):
         '''
-        Using Battaglia et al (2012). Eq. 10. 
+        Using Battaglia et al (2012). Eq. 10.
         Input: Virial Mass in solar mass and Radius in Mpc
         Return: Pressure profile in keV/cm^3 at radius r
         '''
-        
+
         a = cosmo.scale_factor(z)
         rho_critical = cosmo.critical_density(0.5).to(u.M_sun/u.Mpc**3) #In M_sun/Mpc^3
-        
-        
+
+
 
         M200, R200, c200 = mass_adv.changeMassDefinitionCModel(Mvir/cosmo.h, z, 'vir', '200c', c_model = 'ishiyama21')
         #M200, R200, c200 = mass_adv.changeMassDefinitionCModel(Mvir/cosmo_h, z, 'vir', '200c', c_model = 'ishiyama21')
         #M200, R200, c200 = mass_adv.changeMassDefinitionCModel(M500/cosmo_h, z, '500c', '200c', c_model = 'ishiyama21')
         #cvir = concentration.concentration(Mvir, 'vir', z, model = 'ishiyama21')      #Ishiyama et al. (2021)
         #Option to customize concentration, currently default, using Bullock et al. (2001)
- 
+
         M200 *= cosmo.h
         R200 = R200 / 1000 * cosmo.h * (1.+z)
         x = r / R200
-        
+
         G2 = G * 1e-6 / float(Mpc_to_m) * m_sun
         gamma = -0.3
-        
+
         P200 =  G2 * M200 * 200. * rho_critical * (omega_b / omega_m) / 2. / (R200 / (1. + z))    # Msun km^2 / s^2 / Mpc^3
 
         P0 = 18.1 * ((M200 / 1e14)**0.154 * (1. + z)**-0.758)
         xc = 0.497 * ((M200 / 1e14)**-0.00865 * (1. + z)**0.731)
-        beta = 4.35 * ((M200 / 1e14)**0.0393 * (1. + z)**0.415) 
+        beta = 4.35 * ((M200 / 1e14)**0.0393 * (1. + z)**0.415)
         pth = P200 * P0 * (x / xc)**gamma * (1. + (x/xc))**(-1. * beta)      # Msun km^2 / s^2 / Mpc^3
 
         pth *= (m_sun * 1e6 * j_to_kev  / ((Mpc_to_m*100)**3))       # keV/cm^3
@@ -161,12 +160,12 @@ class GenerateCluster():
         if 2 in nums:
             pa = p + '2' + '.png'
             plot_img(y_img, z, path = pa)
-        
+
         if 3 in nums:
             pa = p + '3' + '.png'
             gaussian = gaussian_kernal(0.5, beam_size)
             plot_img(gaussian, z, opt = 2, path = pa)
-        
+
         if 4 in nums:
             pa = p + '4' + '.png'
             plot_img(y_con, z, path = pa)
@@ -187,14 +186,4 @@ class GenerateCluster():
             plot_img(y_noise, z, opt = 3, path = pa) #vizualization starts working from z = 0.115
 
         #return tSZ_signal(z, y_con), tSZ_signal(z, y_noise)
-        return y_img, y_con, cmb_img, noise, cmb_noise, y_noise, SZsignal, aperture  
-
-    
-        
-   
-
-   
- 
-        
-    
-        
+        return y_img, y_con, cmb_img, noise, cmb_noise, y_noise, SZsignal, aperture
